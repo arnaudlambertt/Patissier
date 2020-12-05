9ab6ded381b2663ff54f6d7092eeb51f6663f183
Mathias <mathias.sericola@gmail.com>
2020-12-05 13:23:08 +0100

    Create SceneErreur404.java

---
+++ ProjetJava/src/VIEW/SceneErreur404.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VIEW;
+
+/**
+ *
+ * @author mathi
+ */
+public class SceneErreur404 extends SceneCustom
+{
+    
+    public SceneErreur404()
+    {
+        
+    }
+    
+    @Override
+    public void init()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(View v)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
